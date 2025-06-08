2872a0f37ab6d14b3e89df8cd333a0c362b4222d
Ingo-Braun <ingobraun@hotmail.com>
2025-06-08 18:04:03 -0300

    chore: fix version
    fix version

--- tinyQ.go
+++ tinyQ.go
@@ -17,7 +17,7 @@ import (
 	Subscriber "github.com/Ingo-Braun/TinyQ/subscriber"
 )
 
-const Version = "v0.9.1"
+const Version = "v0.10.1"
 
 // N times witch the router will try to deliver
 // TODO: allow retry count as an configurable variable
